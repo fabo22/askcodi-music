b5e0356190739e69e76264acf5253cce92826775
fabo22 <57267554+fabo22@users.noreply.github.com>
2024-04-25 14:33:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+// You can add any JavaScript functionality here, for example, handling click events, fetching song data from an API, etc.
+// This is a basic example to get you started with the structure.
